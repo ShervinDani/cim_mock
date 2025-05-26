e384d740f4c838347c0b4f37ef8f7a55ebc5ef78
harinithiru-25 <harinit003@gmail.com>
2025-05-26 11:22:37 +0530

    address

--- cim_frontend/src/app/customerform1/customerform1.component.ts
+++ cim_frontend/src/app/customerform1/customerform1.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customerform1',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './customerform1.component.html',
   styleUrl: './customerform1.component.css'
 })
 export class Customerform1Component {
+  addressForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.addressForm = this.fb.group({
+      addressLine1: ['', Validators.required],
+      addressLine2: [''],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      postalCode: ['', Validators.required],
+      country: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.addressForm.valid) {
+      console.log('Address submitted:', this.addressForm.value);
+    } else {
+      this.addressForm.markAllAsTouched(); // Shows all validation errors
+    }
+  }
 }
