89be65061468c8874b0123fe2d443a4d630a2f3f
harinithiru-25 <harinit003@gmail.com>
2025-05-29 09:07:29 +0530

    second

--- cim_frontend/src/app/app.component.ts
+++ cim_frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from "./header/header.component";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
@@ -13,3 +14,27 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class AppComponent {
   title = 'cim_frontend';
 }
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { NgModule } from '@angular/core';
+
+
+// import { HeaderComponent } from "./header/header.component";
+// import { HttpClientModule } from '@angular/common/http';
+// import { FormsModule } from '@angular/forms';  // <-- import FormsModule
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [
+//     RouterOutlet,
+//     HeaderComponent,
+//     HttpClientModule,
+//     FormsModule    // <-- add FormsModule here
+//   ],
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'cim_frontend';
+// }
--- cim_frontend/src/app/statustracking/statustracking.component.spec.ts
+++ cim_frontend/src/app/statustracking/statustracking.component.spec.ts
@@ -1,6 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+// import { StatustrackingComponent } from './statustracking.component';
+
+// describe('StatustrackingComponent', () => {
+//   let component: StatustrackingComponent;
+//   let fixture: ComponentFixture<StatustrackingComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [StatustrackingComponent]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(StatustrackingComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StatustrackingComponent } from './statustracking.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('StatustrackingComponent', () => {
   let component: StatustrackingComponent;
@@ -8,16 +32,23 @@ describe('StatustrackingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StatustrackingComponent]
-    })
-    .compileComponents();
+      declarations: [StatustrackingComponent],
+      imports: [HttpClientTestingModule, FormsModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(StatustrackingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show error if phone number is empty', () => {
+    component.phoneNumber = '';
+    component.checkStatus();
+    expect(component.statusMessage).toBe('Please enter a valid phone number.');
+  });
 });
+
